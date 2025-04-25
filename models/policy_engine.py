36378b64c1bc1bc0e06f2481b5d46e4634fc12e9
Devang Borkar <devangborkar3@gmail.com>
2025-04-25 11:37:08 -0700

    Added mongodb to persist data

--- models/policy_engine.py
+++ models/policy_engine.py
@@ -1,39 +1,107 @@
-
-# Note "Double Check" place 
 from collections import UserDict
 import json
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from models import conflict_class
-from models.capability_lists import CapabilityList
 from models.conflict_class import ConflictClass
+from models.capability_lists import CapabilityList
 from models.dataset import Dataset
 from models.object import Object
-from models.role import Role
+from models.role import Role, Permission
 from models.user import User
-from models.role import Permission
-
-from typing import Tuple
-
+from models.database import Database
+from typing import Tuple, Dict, Any
 
 class PolicyEngine:
-    
-    def __init__(self, conflict_classes=None, datasets=None, objects=None):
+    def __init__(self):
+        self.db = Database()
         self.caps = CapabilityList()
-        self.roles = {}
-        self.users = {}
-        self.objects = {}
-        self.datasets = {}
-        self.conflict_classes = {}
-        self.user_access_history = {}
+        self._load_data()
     
-    def add_user(self, user:User):
-        self.users[user.id] = user
-    
-    def add_role(self, role:Role):
-        self.roles[role.id] = role
+    def _load_data(self):
+        """Load all data from MongoDB into memory"""
+        self.roles = {doc['_id']: Role.from_dict(doc) for doc in self.db.get_all_documents('roles')}
+        self.users = {doc['_id']: User.from_dict(doc) for doc in self.db.get_all_documents('users')}
+        self.objects = {doc['_id']: Object.from_dict(doc) for doc in self.db.get_all_documents('objects')}
+        self.datasets = {doc['_id']: Dataset.from_dict(doc) for doc in self.db.get_all_documents('datasets')}
+        self.conflict_classes = {doc['_id']: ConflictClass.from_dict(doc) for doc in self.db.get_all_documents('conflict_classes')}
         
+        # Load capability lists
+        caps_data = self.db.get_all_documents('capability_lists')
+        if caps_data:
+            self.caps = CapabilityList.from_dict(caps_data[0].get('matrix', {}))
+        
+        # Load access history
+        self.user_access_history = {}
+        for doc in self.db.get_all_documents('access_history'):
+            self.user_access_history[doc['user_id']] = doc.get('accessed_datasets', [])
+    
+    def add_user(self, user: User):
+        self.users[user.id] = user
+        self.db.save_document('users', {'_id': user.id, **user.to_dict()})
+    
+    def add_role(self, role: Role):
+        self.roles[role.id] = role
+        self.db.save_document('roles', {'_id': role.id, **role.to_dict()})
+    
+    def add_object(self, obj: Object):
+        if obj.dataset not in self.datasets:
+            raise Exception(f"Dataset {obj.dataset} does not exist")
+        
+        self.objects[obj.id] = obj
+        if obj.id not in self.datasets[obj.dataset].objects:
+            self.datasets[obj.dataset].objects.append(obj.id)
+            self.db.save_document('datasets', {'_id': obj.dataset, **self.datasets[obj.dataset].to_dict()})
+        
+        self.db.save_document('objects', {'_id': obj.id, **obj.to_dict()})
+    
+    def add_dataset(self, dataset: Dataset):
+        self.datasets[dataset.id] = dataset
+        self.db.save_document('datasets', {'_id': dataset.id, **dataset.to_dict()})
+    
+    def add_conflict_class(self, conflict_class: ConflictClass):
+        self.conflict_classes[conflict_class.class_id] = conflict_class
+        self.db.save_document('conflict_classes', {'_id': conflict_class.class_id, **conflict_class.to_dict()})
+    
+    def assign_role_to_user(self, user_id: str, role_id: str):
+        if user_id not in self.users or role_id not in self.roles:
+            raise ValueError("Invalid user or role ID")
+        
+        if role_id not in self.users[user_id].roles:
+            self.users[user_id].roles.append(role_id)
+            self.db.save_document('users', {'_id': user_id, **self.users[user_id].to_dict()})
+    
+    def grant_direct_permission(self, user_id: str, object_id: str, action: str):
+        if user_id not in self.users or object_id not in self.objects:
+            raise ValueError("Invalid user or object ID")
+        
+        self.caps.add_permission(user_id, object_id, action)
+        self.db.save_document('capability_lists', {'_id': 'caps_matrix', 'matrix': self.caps.to_dict()})
+        return True
+    
+    def record_access(self, user_id: str, object_id: str):
+        if user_id not in self.users or object_id not in self.objects:
+            raise ValueError("Invalid user or object ID")
+        
+        obj = self.objects[object_id]
+        dataset_id = obj.dataset
+        
+        if user_id not in self.user_access_history:
+            self.user_access_history[user_id] = []
+        
+        if dataset_id not in self.user_access_history[user_id]:
+            self.user_access_history[user_id].append(dataset_id)
+            self.db.save_document('access_history', {
+                '_id': user_id,
+                'user_id': user_id,
+                'accessed_datasets': self.user_access_history[user_id]
+            })
+            
+            if dataset_id not in self.users[user_id].access_history:
+                self.users[user_id].access_history.append(dataset_id)
+                self.db.save_document('users', {'_id': user_id, **self.users[user_id].to_dict()})
+        return True
+    
     def add_permission_to_role(self, role_id, object_id, action):
         """
         Adds a permission to a role.
@@ -45,6 +113,7 @@ class PolicyEngine:
             
         permission = Permission(object_id=object_id, action=action)
         self.roles[role_id].permissions.append(permission)
+        self.db.save_document('roles', {'_id': role_id, **self.roles[role_id].to_dict()})
         return True
         
     def revoke_permission_from_role(self, role_id, object_id, action):
@@ -59,44 +128,15 @@ class PolicyEngine:
         role.permissions = [p for p in role.permissions 
                            if not (p.object_id == object_id and p.action == action)]
         
+        self.db.save_document('roles', {'_id': role_id, **role.to_dict()})
         return len(role.permissions) < original_length
-    
-    
-    def add_dataset(self, dataset:Dataset):
-        self.datasets[dataset.id] = dataset
-    
-    def add_conflict_class(self, conflict_class:ConflictClass):
-        self.conflict_classes[conflict_class.class_id] = conflict_class
-    
-    def assign_role_to_user(self, user_id, role_id):
-        if user_id not in self.users or role_id not in self.roles:
-            raise ValueError("Invalid user or role ID")
-        
-        if role_id not in self.users[user_id].roles:
-            self.users[user_id].roles.append(role_id)
-    
-        # if role_id in self.users[user_id].roles:
-        #     self.users[user_id].roles.append(role_id)
-    
-    def remoke_role_from_user(self, user_id, role_id):
-        if user_id not in self.users or role_id not in self.roles:
-            raise ValueError("Invalid user or role ID")
-        
-        if role_id in self.users[user_id].roles:
-            self.users[user_id].roles.remove(role_id)
-            
-    def grant_direct_permission(self, user_id, object_id, action):
-        if user_id not in self.users or object_id not in self.objects:
-            raise ValueError("Invalid user or object ID")
-        
-        self.caps.add_permission(user_id, object_id, action)
-        return True
     
     def revoke_direct_permission(self, user_id, object_id, action):
         if user_id not in self.users or object_id not in self.objects:
             raise ValueError("Invalid user or object ID")
     
         self.caps.remove_permission(user_id, object_id, action)
+        self.db.save_document('capability_lists', {'_id': 'caps_matrix', 'matrix': self.caps.to_dict()})
         
         # Check if user still has this permission through roles
         rbac_allowed, _ = self._check_rbac(user_id, object_id, action)
@@ -107,22 +147,13 @@ class PolicyEngine:
             
         return True
     
-    
-    def add_object(self, obj:Object):
-        '''
-        Adds an object to the policy engine.
-        If the dataset for the object does not exist, it willl be created.
-    
-        '''
-        self.objects[obj.id] = obj
-        if obj.dataset in self.datasets: # Check if the dataset exists
-            # Add the object to the dataset if it does not exist
-            if obj.id not in self.datasets[obj.dataset].objects:
-                self.datasets[obj.dataset].objects.append(obj.id)
-            
-        else: # Double check
-            # Raise an exception if the dataset does not exist
-            raise Exception(f"Dataset {obj.dataset} does not exist")
+    def remoke_role_from_user(self, user_id, role_id):
+        if user_id not in self.users or role_id not in self.roles:
+            raise ValueError("Invalid user or role ID")
+        
+        if role_id in self.users[user_id].roles:
+            self.users[user_id].roles.remove(role_id)
+            self.db.save_document('users', {'_id': user_id, **self.users[user_id].to_dict()})
     
     def _check_chinese_wall(self, user_id, object_id):
         if object_id not in self.objects:
@@ -203,24 +234,6 @@ class PolicyEngine:
             
         return False, "No permission found"
     
-    def record_access(self, user_id, object_id):
-        if user_id not in self.users or object_id not in self.objects:
-            raise ValueError("Invalid user or object ID")
-    
-        obj = self.objects[object_id]
-        dataset_id = obj.dataset
-    
-        if user_id not in self.user_access_history:
-            self.user_access_history[user_id] = []
-        
-        if dataset_id not in self.user_access_history[user_id]:
-            self.user_access_history[user_id].append(dataset_id)
-    
-            # Update the user's access history in the User module
-            if dataset_id not in self.users[user_id].access_history:
-                self.users[user_id].access_history.append(dataset_id)
-        return True
-        
     def grant_access(self, user_id: str, object_id: str, action: str) -> Tuple[bool, str]:
     
         allowed, reason = self.check_access(user_id, object_id, action)
